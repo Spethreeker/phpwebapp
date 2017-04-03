9fa95f5e5ff799847b3d65f56e70c3ad938212da
Spencer Allegrezza <sallegrezz@gmail.com>
2017-04-03 18:00:55 -0500

    Aaaa

--- makelog.php
+++ makelog.php
@@ -1,11 +1,15 @@
 <?php 
-   
+
+   function fail($pub, $pvt = '')
+    {
+        $msg = $pub;
+        if ($pvt !== '')
+            $msg .= ": $pvt";
+        exit("An error occurred $msg.\n");
+    }
     
     
-    $clientName = $_POST['clientname'];
-    $description = $_POST['description'];
-    $date = $_POST['date'];
-    $clientPhone = $_POST['clientphone'];
+    
     $db_host = 'localhost';
     $db_user = 'root';
     $db_pass = 'root';
@@ -15,19 +19,20 @@
     $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
     if (mysqli_connect_errno())
         fail('MySQL connect', mysqli_connect_error());
-    $sql = "INSERT INTO logs (clientName, description) values ('$clientName', '$date')";
-    // ($stmt = $db->prepare('insert into logs (clientName, description, date, clientPhone) values (?, ?, ?, ?)'))
-    //     || fail('MySQL prepare', $db->error);
-    // $stmt->bind_param('ss', $user, $hash)
-    //     || fail('MySQL bind_param', $db->error);
-    // $stmt->execute()
-    //     || fail ('MySQL execute', $db->error);
-    // $stmt->close();
-   
-    ?>
-    <html>
-<body>
-    <p>Worked</p>
-    </body>
 
-        </html>
+    $clientName = $_POST['clientname'];
+    $description = $_POST['description'];
+    $date = $_POST['date'];
+    $clientPhone = $_POST['clientphone'];
+    echo($clientName);
+    // $sql = "INSERT INTO logs (clientName, description) VALUES ('$clientName', '$date')";
+    ($stmt = $db->prepare('insert into logs (clientName, description) values (?, ?)'))
+        || fail('MySQL prepare', $db->error);
+    $stmt->bind_param('ss', $clientName, $description)
+        || fail('MySQL bind_param', $db->error);
+    $stmt->execute()
+        || fail ('MySQL execute', $db->error);
+    $stmt->close();
+   echo('Done!');
+    ?>
+  <a href="home.html">Go Back</a>
