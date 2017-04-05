b200a27f91700050fb7a145c017e746dc560b496
Spethreeker <sallegrezz@gmail.com>
2017-04-05 09:25:40 -0500

    added buttons instead of menu

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 function showlog() {
              if ($("#log-form").is(":hidden")){
                  $("#log-form").slideDown();
+                 $("#add-log-button").toggleClass("red");
             }else{
                  $("#log-form").slideUp();
        
