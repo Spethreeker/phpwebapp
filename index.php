6e1975b6a1635cc5cfd04566dd79123dde57b8fb
Spencer Allegrezza <sallegrezz@gmail.com>
2017-05-25 00:33:37 -0500

    Login and register are good

--- index.php
+++ index.php
@@ -2,57 +2,81 @@
 <?php
   session_start();
 ?>
-<html>
+<html class="blue">
   <head>
    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="css/bulma.css">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
      <script src="js/jquery-3.2.0.min.js"></script>
      <script src="js/parsley.min.js"></script>
-     <script src="js/scripts.js"></script>
-     <script>
-     
-     </script>
+     <style>
+    #register-link-button:hover{
+      background: #eee;
+    }
+       </style>
   </head>
 </html>
-<body>
-<div class="container notification">
+<body class="index-body animated" id="index-body">
+<div class="container">
   <div class="hero">
-    <div class="hero-body">
-      <span><img class="image is-128x128" src="hammerpen.png"/>
-      <h2 class="title">Worklogs</h2></span>
-      <p class="subtitle">A work journal on the Internet</p>
+    <div class="hero-header">&nbsp;</div>
+    <div class="hero-body animated" id="hero-body">
+      <img class="image is-128x128 margin-auto-centered" src="hammerpen.png" alt="Worklogs" id="logo"/>
+      <div class="has-text-centered">
+        <h1 class="title is-2 white-font">Worklogs</h1>
+        <p class="subtitle is-4 off-white-font">A work journal on the Internet</p>
+      </div>
     </div>
-  </div>
-  <div class="columns">
-    <div  class="column">
-     <form id='login' class="notification" action="user-login.php" name="loginform" method='post' accept-charset='UTF-8' data-parsley-validate>
-     <input type="hidden" name="op" value="login">
-     <div class="field">
+    <div class="hero-foot">&nbsp;</div>
+</div>
+   <div class="card animated index-form margin-auto-centered" id="login-form">
+       <h1 class="card-header-title">Login</h1>
+     <form id='login' action="user-login.php" class="card-content" name="loginform" method='post' accept-charset='UTF-8' data-parsley-validate>
+       <input type="hidden" name="op" value="login">
+       <div class="field">
        <label for='email' class="label heading light-gray" >Email Address</label>
-     <span class="control">  <input type='email' class="input" name='email' id='Email'  required/></span>
+        <span class="control">  <input type='email' class="input" name='email' id='Email'  required/></span>
+       </div>
+       <div class="field">
+       <label class="field-label label heading light-gray"  for='password' >Password</label>
+          <span class="control">
+          <input class="input"  type='password' name='password' id='password' required/>
+        </span>
+       </div>
+     <div class="field is-fullwidth">
+       <label class="subtitle is-6">&nbsp;</label>
+       <span class="control">
+        <input class="button blue is-medium is-fullwidth"  type='submit' name='Submit' value='Login'>
+       </span>
      </div>
-     <div class="field">
-       <label class="label heading light-gray"  for='password' >Password</label>
-       <input class="input"  type='password' name='password' id='password' required/>
-     </div>
+   </form>
+   <div class="card-footer">
+   <div class="card-footer-item">
+   <a href="register.php" class="button is-link" id="register-link-button">Or register an account!<i class="em em-ok_hand"></i></a>
+   </div>
+   </div>
+</div>
      <?php
       if(!isset($_SESSION['result'])) {
                             echo("<br>");
                         }else{
                             echo $_SESSION['result']."<br>";
-                        }
-   ?>
-     <input class="button light-blue"  type='submit' name='Submit' value='Login'>
-   
-   </form>
-  </div>
-   <div class="column">
-    <h3 class="title light-gray">Or, if you don't have an account:</h3>
-    <a class="button light-blue" href="register.php">Register</a>
-   </div>
- </div>
-</div>
-</body>
+      }?>
 
+</div>
+  <footer class="footer blue">
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Privacy Policy</a>
+  </footer>
+</body>
+     <script src="js/scripts.js"></script>
+     <script>
+    
+      var index = document.getElementById('index-body');
+      window.addEventListener('load', 
+        function() { 
+         index.classList.add('fadeIn');
+        }, false);
+    
+    </script>
 </html>
--- register.php
+++ register.php
@@ -4,37 +4,73 @@ session_start();
 $_SESSION['message'] = '';
 ?>
 
-<html>
+<html class="blue">
     <head>
         <link rel="stylesheet" href="css/bulma.css">
         <link rel="stylesheet" href="css/style.css">
+        <style> #login-link-button:hover{ background: #eee; }</style>
     </head>
 </html>
-<body>
-<div class="container notification">
-  <div class="columns">
-            <div class="column is-half">
-            <form id='register' name="register" class="notification" action='user-man.php' method='post' accept-charset='UTF-8'>
-                <legend>Register</legend>
-                <input type="hidden" name="op" value="new">
-                <label for='FirstName' class="label heading">First Name</label>
-                    <input type='text' class="input is-block" name='name' id='name' maxlength="50" required/>
-                <label for='Email' class="label heading" >Email Address</label>
-                    <input type='email' class="input" name='email' id='Email' maxlength="50" required/>
-                <label class="label heading"  for='password' >Password</label>
-                  <input class="input"  type='password' name='password' id='password' maxlength="50" required/>
-                    <input class="button blue"  type='submit' name='Submit' value='Submit'>
+<body class="index-body animated" id="index-body">
+    <div class="hero">
+    <div class="hero-header">&nbsp;</div>
+    <div class="hero-body animated" id="hero-body">
+      <img class="image is-128x128 margin-auto-centered" src="hammerpen.png" alt="Worklogs" id="logo"/>
+      <div class="has-text-centered">
+        <h1 class="title is-2 white-font">Worklogs</h1>
+        <p class="subtitle is-4 off-white-font">A work journal I hope you'll like</p>
+      </div>
+    </div>
+    <div class="hero-foot">&nbsp;</div>
+</div>
+<div class="container">
+  <div class="card animated index-form" id="register-form">
+    <h1 class="card-header-title">Register</h1>
+    <form id='register' name="register" class="card-content" action='user-man.php' method='post' accept-charset='UTF-8'>
+      <input type="hidden" name="op" value="new">
+      <div class="field">
+      <label for='FirstName' class="label heading">First Name</label>
+        <div class="control">
+          <input type='text' class="input is-block" name='name' id='name' maxlength="50" required/>
+        </div>
+      </div>
+      <div class="field">
+      <label for='Email' class="label heading" >Email Address</label>
+          <div class="control">
+            <input type='email' class="input" name='email' id='Email' maxlength="50"/>
+            </div>
+          </div>
+          <div class="field">
+            <label class="label heading"  for='password' >Password</label>
+          <div class="control">
+            <input class="input"  type='password' name='password' id='password' maxlength="50"/>
 
-            
+          </div>
+        </div>
+        <div class="field is-fullwidth">
+          <label class="subtitle is-6">&nbsp;</label>
+          <div class="control">
+            <input class="button blue is-medium is-fullwidth"  type='submit' name='Submit' value='Register'>
+          </div>
+        </div>
   </form>
-  <a class="button blue" href="home.html">Home</a>
+<div class="card-footer">
+     <div class="card-footer-item">
+     <a href="index.php" class="button is-link" id="login-link-button">Back to the login page</a>
      </div>
-
-    
   </div>
-  
-
-
+</div>
+  </div>
+  <footer class="footer blue"></footer>
 </body>
-
+ <script src="js/scripts.js"></script>
+     <script>
+    
+      var index = document.getElementById('index-body');
+      window.addEventListener('load', 
+        function() { 
+         index.classList.add('fadeIn');
+        }, false);
+    
+    </script>
 </html>
